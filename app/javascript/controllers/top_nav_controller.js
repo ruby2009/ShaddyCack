a0b376677d639ea6a6d6e4d1aab8dac42e182143
ruby2009 <bgcall@indiana.edu>
2022-05-22 15:44:25 -0400

    added button depth

--- app/javascript/controllers/top_nav_controller.js
+++ app/javascript/controllers/top_nav_controller.js
@@ -2,13 +2,17 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="top-nav"
 export default class extends Controller {
-  static targets = ['hole', 'leadercard']
+  static targets = ['hole', 'leadercard', 'scoreNav', 'leadercardNav']
 
   showScore () {
-    console.log("in show score")
     if(this.holeTarget.classList.contains('hidden')) {
       this.holeTarget.classList.remove('hidden')
       this.leadercardTarget.classList.add('hidden')
+    }
+
+    if(this.leadercardNavTarget.classList.contains('shadow-inner')) {
+      this.leadercardNavTarget.classList.remove('shadow-inner')
+      this.scoreNavTarget.classList.add('shadow-inner')
     }
   }
 
@@ -16,6 +20,12 @@ export default class extends Controller {
     if(this.leadercardTarget.classList.contains('hidden')) {
       this.leadercardTarget.classList.remove('hidden')
       this.holeTarget.classList.add('hidden')
+    }
+
+    console.log(this.scoreNavTarget, "score nav?")
+    if(this.scoreNavTarget.classList.contains('shadow-inner')) {
+      this.scoreNavTarget.classList.remove('shadow-inner')
+      this.leadercardNavTarget.classList.add('shadow-inner')
     }
   }
 }
